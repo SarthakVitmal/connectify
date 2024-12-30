f626a88a32e1daac39b5615249b848213e3e2e6f
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2024-12-30 23:50:51 +0530

    Comment out user testimonials section in LandingPage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,7 +99,7 @@ export default function LandingPage() {
           </div>
         </section>
 
-        <section className="py-20 bg-white">
+        {/* <section className="py-20 bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h2 className="text-3xl font-extrabold text-indigo-800 text-center mb-12">
               What Our Users Say
@@ -127,7 +127,7 @@ export default function LandingPage() {
               ))}
             </div>
           </div>
-        </section>
+        </section> */}
       </main>
 
       <footer className="bg-indigo-900 text-indigo-100">
